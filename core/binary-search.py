d09a6a57fdf8f0afeb8a07bf0c63a35ca7323626
fillipe soranso <fillipe.soranso@gmail.com>
2026-02-02 21:56:48 -0300

    feat: implement binary search in python

---
+++ core/binary-search.py
@@ -0,0 +1,21 @@
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = arr[mid]
+        if guess == target:
+            return mid
+        if guess > target:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+my_arr = [1, 3, 5, 7, 9]
+
+print(binary_search(my_arr, 9)) # 4
+print(binary_search(my_arr, 10)) # None
+
+        
